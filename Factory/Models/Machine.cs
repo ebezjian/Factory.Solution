da4cc4f3a23c5498d799107207a972560d6ec9ab
Ellie Lambert <ebezjian@gmail.com>
2021-01-08 12:05:48 -0500

    add functionality to Machine Models

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.Engineers = new HashSet<EngineerMachine>();
+    }
+    public int MachineId { get; set; }
+    public string MachineName { get; set; }
+    public ICollection<EngineerMachine> Engineers { get; }
+  }
+}
